f72fbc0838bd6da6e749ab376c43185d533d5e92
Jordan Long <jordanleelong5@gmail.com>
2019-09-21 18:01:54 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,8 +11,21 @@ end
 
 
 def find_max_value(array)
+  x = array[0]
+  array.length.times { |index|
+    if array[index] > x
+      x = array[index]
+    end
+  }
+  x
 end
 
 def find_min_value(array)
-  # Add your solution here
+  x = array[0]
+  array.length.times { |index|
+    if array[index] < x
+      x = array[index]
+    end
+  }
+  x
 end
