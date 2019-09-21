65a3f8e4bf774a6c260ecb1960c249e4765b4d50
Jordan Long <jordanleelong5@gmail.com>
2019-09-21 17:50:53 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -6,12 +6,12 @@ def find_element_index(array, value_to_find)
   end
     count += 1
     end
-    nil
-  end
+  nil
+end
 
 
 def find_max_value(array)
-
+end
 
 def find_min_value(array)
   # Add your solution here
