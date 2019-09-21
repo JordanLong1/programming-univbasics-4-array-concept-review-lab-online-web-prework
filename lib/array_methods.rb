c47a036c4a1b2ccf02d5abefdb3bd629d1f4647f
Jordan Long <jordanleelong5@gmail.com>
2019-09-21 17:59:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,6 +11,13 @@ end
 
 
 def find_max_value(array)
+  x = array[0]
+  array.length.times { |index|
+    if array[index] > x
+      x = array[index]
+    end
+  }
+  x
 end
 
 def find_min_value(array)
